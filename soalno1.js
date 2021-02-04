b472f2de7087e33afa62e4f0db2935c2b2b430f4
Brianrahmarela <brian.rahmarela@gmail.com>
2021-02-04 12:02:25 +0700

    update js

--- soalno1.js
+++ soalno1.js
@@ -1,25 +1,11 @@
-//sblmnya
-// function voidEven(n){
-//   //cek angka genap & >= 0
-//   if(n%2 == 0 && n >= 0) {
-//     console.log(n);
-//     voidEven(n-2);
-//   } else if (n%2 == 1 && n >= 0){
-//     voidEven(n-1);
-//     // console.log(n)
-//   }
-// }
 
-// console.log(voidEven(8));
-// console.log(voidEven(5));
 
 function voidEven(n){
   //cek angka genap & >= 0
   if(n%2 == 0 && n >= 0) {
     // console.log(n);
     voidEven(n-2);
-    // document.write(`function void hasilnya: ${n}`)
-    // document.write(`hasil:`); 
+
     document.write(n); 
   } else if (n%2 == 1 && n >= 0){
     voidEven(n-1);
